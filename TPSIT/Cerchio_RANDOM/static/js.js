e78db56b5b7d5a202ef5e6cb06cc004ab45bc0a5
DeeYh <102679066+DeeYh0@users.noreply.github.com>
2022-11-11 13:18:13 +0100

    Update js.js

--- TPSIT/Cerchio_RANDOM/static/js.js
+++ TPSIT/Cerchio_RANDOM/static/js.js
@@ -2,39 +2,39 @@ function setup()
 {
     createCanvas(600, 300)
     background(0, 210, 0)
-    frameRate(5)
+    frameRate(70)
+    
 }
 
 
 function draw()
 {
-    let witdh = random(600)
-    let height = random(300)
-    let diameter = random(30 , 90)
-    let r = random(0 , 255)
-    let g = random(0 , 255)
-    let b = random(0 , 255)
+    if(frameCount<=1000)
+    {
+        fill("white")
+        rect(520,120, 64,40 );
+        let witdh = random(450)
+        let height = random(300)
+        let diameter = random(30 , 90)
+        let r = random(0 , 255)
+        let g = random(0 , 255)
+        let b = random(0 , 255)
+        
+        fill(r, g, b)
+        circle(witdh, height, 100)
+
+
+        //frame counter
+        textSize(30);
+        textAlign(CENTER);
+        fill("black");
+        text(frameCount, 550, 150);
+        
+    }
     
-    fill(r, g, b)
-    circle(witdh, height, 100)
-}
-
-
-
-function drawFlower(x, y, diameter, radius)
-{
-    
-    
-    strokeWeight(3)
-
-    fill(r, g, b) //cerchi intorno 
-    circle(width - radius, height - radius, diameter)
-    circle(width + radius, height + radius, diameter)
-    circle(width - radius, height + radius, diameter)
-    circle(width + radius, height - radius, diameter)
-
-    fill(r, g, b)  //cerchio centrale
-    circle(width, height, diameter)
-
-
+    else
+    {
+        frameCount=0
+        background(0, 210, 0)
+    }
 }
