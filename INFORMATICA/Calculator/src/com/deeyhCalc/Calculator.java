8db3958cfafe1f1505dab59e6ba9c4824c056820
DeeYh <102679066+DeeYh0@users.noreply.github.com>
2023-02-11 13:16:53 +0100

    Add Calculator

---
+++ INFORMATICA/Calculator/src/com/deeyhCalc/Calculator.java
@@ -0,0 +1,364 @@
+package com.deeyhCalc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JTextPane;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+public class Calculator {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculator window = new Calculator();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Calculator() {
+		initialize();
+	}
+	public float num1 = 0;
+	public float num2 = 0;
+	public float result = 0;
+	public String operations;
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 350, 510);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		
+		final JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(new Color(242, 242, 242));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 45));
+		lblNewLabel.setBounds(10, 77, 314, 101);
+		frame.getContentPane().add(lblNewLabel);
+		
+
+		
+		final JButton btnNewButton_9 = new JButton("0");
+		btnNewButton_9.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9.getText();
+				lblNewLabel.setText(Enternumber);
+				
+			}
+		});
+		btnNewButton_9.setBackground(new Color(192, 192, 192));
+		btnNewButton_9.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9.setBounds(10, 405, 119, 55);
+		frame.getContentPane().add(btnNewButton_9);
+		
+		JButton btnNewButton_9_1 = new JButton("=");
+		btnNewButton_9_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String answer;
+				num2 = Float.parseFloat(lblNewLabel.getText());
+				if(operations == "+")
+				{
+					result = num1 + num2;
+					answer = String.format("%.2f", result);
+					lblNewLabel.setText(answer);
+				}
+				else if(operations == "-")
+				{
+					result = num1 - num2;
+					answer = String.format("%.2f", result);
+					lblNewLabel.setText(answer);
+				}
+				else if(operations == "*")
+				{
+					result = num1 * num2;
+					answer = String.format("%.2f", result);
+					lblNewLabel.setText(answer);
+				}
+				else if(operations == "/")
+				{
+					result = num1 / num2;
+					answer = String.format("%.2f", result);
+					lblNewLabel.setText(answer);
+				}
+				else if(operations == "%")
+				{
+					result = num1 % num2;
+					answer = String.format("%.2f", result);
+					lblNewLabel.setText(answer);
+				}
+				else if (operations == "√") {
+				    result = (float) Math.sqrt(num1);
+				    num2 = 0;
+				    answer = String.format("%.2f", result);
+				    lblNewLabel.setText(answer);
+				}
+				
+			}
+		});
+		btnNewButton_9_1.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnNewButton_9_1.setBounds(269, 339, 55, 121);
+		frame.getContentPane().add(btnNewButton_9_1);
+		
+		final JButton btnNewButton_9_1_1 = new JButton(".");
+		btnNewButton_9_1_1.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1.getText();
+				lblNewLabel.setText(Enternumber);	
+			}
+		});
+		btnNewButton_9_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1.setBounds(139, 405, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1);
+		
+		JButton btnNewButton_9_1_2 = new JButton("+");
+		btnNewButton_9_1_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				num1 = Float.parseFloat(lblNewLabel.getText());
+				lblNewLabel.setText("");
+				operations = "+";
+				
+			}
+		});
+		btnNewButton_9_1_2.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnNewButton_9_1_2.setBounds(204, 405, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_2);
+		
+		JButton btnNewButton_9_1_1_2 = new JButton("-");
+		btnNewButton_9_1_1_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				num1 = Float.parseFloat(lblNewLabel.getText());
+				lblNewLabel.setText("");
+				operations = "-";
+			}
+		});
+		btnNewButton_9_1_1_2.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_1_2.setFont(new Font("MS UI Gothic", Font.BOLD, 30));
+		btnNewButton_9_1_1_2.setBounds(204, 339, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_2);
+		
+		JButton btnNewButton_9_1_1_2_1 = new JButton("x");
+		btnNewButton_9_1_1_2_1.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_1_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				num1 = Float.parseFloat(lblNewLabel.getText());
+				lblNewLabel.setText("");
+				operations = "*";
+			}
+		});
+		btnNewButton_9_1_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_2_1.setBounds(204, 273, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_2_1);
+		
+		JButton btnNewButton_9_1_1_2_2 = new JButton("/");
+		btnNewButton_9_1_1_2_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				num1 = Float.parseFloat(lblNewLabel.getText());
+				lblNewLabel.setText("");
+				operations = "/";
+			}
+		});
+		btnNewButton_9_1_1_2_2.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_1_2_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_2_2.setBounds(204, 207, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_2_2);
+		
+		JButton btnNewButton_9_1_1_2_3 = new JButton("%");
+		btnNewButton_9_1_1_2_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				num1 = Float.parseFloat(lblNewLabel.getText());
+				lblNewLabel.setText("");
+				operations = "%";
+				
+			}
+		});
+		btnNewButton_9_1_1_2_3.setBackground(new Color(225, 225, 225));
+		btnNewButton_9_1_1_2_3.setFont(new Font("Nyala", Font.PLAIN, 30));
+		btnNewButton_9_1_1_2_3.setBounds(269, 273, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_2_3);
+		
+		final JButton btnNewButton_9_1_1_1 = new JButton("1");
+		btnNewButton_9_1_1_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_1.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_1.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_1.setBounds(10, 339, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_1);
+		
+		final JButton btnNewButton_9_1_1_3 = new JButton("2");
+		btnNewButton_9_1_1_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_3.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_3.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_3.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_3.setBounds(74, 339, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_3);
+		
+		
+		
+		final JButton btnNewButton_9_1_1_4 = new JButton("3");
+		btnNewButton_9_1_1_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_4.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_4.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_4.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_4.setBounds(139, 339, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_4);
+		
+		final JButton btnNewButton_9_1_1_5 = new JButton("6");
+		btnNewButton_9_1_1_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_5.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_5.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_5.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_5.setBounds(139, 273, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_5);
+		
+		final JButton btnNewButton_9_1_1_6 = new JButton("5");
+		btnNewButton_9_1_1_6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_6.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_6.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_6.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_6.setBounds(74, 273, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_6);
+		
+		final JButton btnNewButton_9_1_1_7 = new JButton("4");
+		btnNewButton_9_1_1_7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_7.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_7.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_7.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_7.setBounds(10, 273, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_7);
+		
+		JButton btnNewButton_9_1_1_10 = new JButton("C");
+		btnNewButton_9_1_1_10.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lblNewLabel.setText("");
+			}
+		});
+		btnNewButton_9_1_1_10.setBackground(new Color(255, 83, 83));
+		btnNewButton_9_1_1_10.setFont(new Font("Tahoma", Font.BOLD, 23));
+		btnNewButton_9_1_1_10.setBounds(269, 207, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_10);
+		
+		final JButton btnNewButton_9_1_1_5_1 = new JButton("9");
+		btnNewButton_9_1_1_5_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_5_1.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_5_1.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_5_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_5_1.setBounds(139, 207, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_5_1);
+		
+		final JButton btnNewButton_9_1_1_5_2 = new JButton("8");
+		btnNewButton_9_1_1_5_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_5_2.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_5_2.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_5_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_5_2.setBounds(74, 207, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_5_2);
+		
+		final JButton btnNewButton_9_1_1_5_3 = new JButton("7");
+		btnNewButton_9_1_1_5_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String Enternumber = lblNewLabel.getText() + btnNewButton_9_1_1_5_3.getText();
+				lblNewLabel.setText(Enternumber);
+			}
+		});
+		btnNewButton_9_1_1_5_3.setBackground(new Color(192, 192, 192));
+		btnNewButton_9_1_1_5_3.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_9_1_1_5_3.setBounds(10, 207, 55, 55);
+		frame.getContentPane().add(btnNewButton_9_1_1_5_3);
+		
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBounds(10, 77, 314, 101);
+		frame.getContentPane().add(editorPane);
+		
+		JLabel lblNewLabel_1 = new JLabel("CALCULATOR 2000");
+		lblNewLabel_1.setBackground(new Color(255, 83, 83));
+		lblNewLabel_1.setFont(new Font("Serif", Font.PLAIN, 35));
+		lblNewLabel_1.setBounds(10, 11, 314, 49);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		
+	}
+}
---
+++ INFORMATICA/Calculator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author VitoGabriele
+ *
+ */
+module Calculator {
+}
