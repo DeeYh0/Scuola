f6406649a50c62aae31907929b2c758649208dc6
DeeYh <102679066+DeeYh0@users.noreply.github.com>
2023-04-27 10:27:15 +0200

    Add some FXML

--- INFORMATICA/StudioDentistico/src/main/java/com/deeyh/studiodentistico/StudioApplication.java
+++ INFORMATICA/StudioDentistico/src/main/java/com/deeyh/studiodentistico/StudioApplication.java
@@ -1,19 +1,18 @@
 package com.deeyh.studiodentistico;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.image.Image;
+        import javafx.scene.image.ImageView;
+        import javafx.stage.Stage;
 
 
-import java.io.IOException;
+        import java.io.IOException;
 
 public class StudioApplication extends Application {
-    Stage stage;
     @Override
-    public void start() throws IOException {
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StudioApplication.class.getResource("Studio-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Studio Dentistico");
--- INFORMATICA/StudioDentistico/src/main/java/com/deeyh/studiodentistico/StudioController.java
+++ INFORMATICA/StudioDentistico/src/main/java/com/deeyh/studiodentistico/StudioController.java
@@ -3,13 +3,10 @@ package com.deeyh.studiodentistico;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class StudioController {
-
     public void close()
     {
         System.exit(0);
@@ -26,7 +23,6 @@ public class StudioController {
             ab.show();
             ab.setResizable(false);
 
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,6 +34,56 @@ public class StudioController {
         Stage stage = new Stage();
         stage.setScene(new Scene(root1));
         stage.show();
-
     }
+
+    public void registra() throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Registra.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+            Stage reg = new Stage();
+            reg.setTitle("Registra Pazienti");
+            reg.setScene(scene);
+            reg.show();
+            reg.setResizable(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void visualizza() throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Visualizza.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+            Stage vis = new Stage();
+            vis.setTitle("Visualizza Pazienti");
+            vis.setScene(scene);
+            vis.show();
+            vis.setResizable(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void gestione() throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Gestione.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+            Stage ges = new Stage();
+            ges.setTitle("Gestione Pazienti");
+            ges.setScene(scene);
+            ges.show();
+            ges.setResizable(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
