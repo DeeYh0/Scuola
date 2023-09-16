a8904aefa1b63d46016d6bbf4768696e698588e8
DeeYh <102679066+DeeYh0@users.noreply.github.com>
2023-09-16 13:42:39 +0200

    add map

---
+++ TPSIT/Weather_Forecast/static/js.js
@@ -0,0 +1,7 @@
+let map = L.map('map').setView([41.9027835, 12.4963655], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
