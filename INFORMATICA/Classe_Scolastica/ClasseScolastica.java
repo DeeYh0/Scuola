699bf7a2be5049bbe03b3c2e73f682dcb2840aa3
DeeYh <102679066+DeeYh0@users.noreply.github.com>
2023-09-21 18:40:00 +0200

    Add classescolastica and chart

---
+++ INFORMATICA/Classe_Scolastica/ClasseScolastica.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClasseScolastica {
+    public static void main(String[] args) {
+
+        ArrayList<Studente> classe = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Inserisci anagrafica studente");
+            System.out.println("2. Stampa classe");
+            System.out.println("3. Ricerca per nome e cognome");
+            System.out.println("4. Esci");
+            System.out.print("Scelta: \n");
+
+            int scelta = input.nextInt();
+            input.nextLine();
+
+            switch (scelta) {
+                case 1:
+                    System.out.print("Inserisci nome: ");
+                    String nome = input.nextLine();
+
+                    System.out.print("Inserisci cognome: ");
+                    String cognome = input.nextLine();
+
+                    System.out.print("Inserisci età: ");
+                    int eta = input.nextInt();
+                    
+                    classe.add(new Studente(nome, cognome, eta));
+                    break;
+
+                case 2:
+                    System.out.println("\nElenco studenti: \n");
+                    if (classe.isEmpty()) {
+                        System.out.println("Non ci sono studenti registrati al momento!\n");
+                    } else {
+                        for (Studente studente : classe) {
+                            System.out.println(studente + "\n");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("\nInserisci nome da cercare: ");
+                    String nomeDaCercare = input.nextLine();
+                    System.out.print("Inserisci cognome da cercare: ");
+                    String cognomeDaCercare = input.nextLine();
+
+                    boolean trovato = false;
+                    for (Studente studente : classe) {
+                        if (studente.getNome().equals(nomeDaCercare) && studente.getCognome().equals(cognomeDaCercare)) {
+                            System.out.println("\nStudente trovato: " + studente + "\n");
+                            trovato = true;
+                            break;
+                        }
+                    }
+
+                    if (!trovato) {
+                        System.out.println("\nStudente non trovato.\n");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("\nUscita dal programma.\n");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Scelta non valida. Riprova.");
+                    break;
+            }
+        }
+    }
+}
--- TPSIT/Weather_Forecast/static/js.js
+++ TPSIT/Weather_Forecast/static/js.js
@@ -1,52 +1,66 @@
-let marker;  //marker con coordinate
+let marker;
 let minTemperature;
 let maxTemperature;
-let map; 
+let map;
+let chartData = {
+    labels: [],
+    temperature: [],
+    humidity: [],
+};
 
 function getWeather(latitude, longitude) {
     return fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_min,temperature_2m_max`)
         .then(response => response.json());
-    }
+}
 
-function createMap (lat,lng)
-{
+function createMap(lat, lng) {
     map = L.map("map").setView([41.9027835, 12.4963655], 6);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-marker = L.marker([lat, lng]).addTo(map);  //POPUP MARKER
-marker.bindPopup(`POSIZIONE MARKER<br>Latitude: ${lat.toFixed(5)}<br>Longitude: ${lng.toFixed(5)}<br>Min Temperature: ${minTemperature}°C<br>Max Temperature: ${maxTemperature}°C`).openPopup(); //non sapevo come fare oltre al <br>
+    marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup(`POSIZIONE MARKER<br>Latitude: ${lat.toFixed(5)}<br>Longitude: ${lng.toFixed(5)}<br>Min Temperature: ${minTemperature}°C<br>Max Temperature: ${maxTemperature}°C`).openPopup(); //ho usato <br>... non sapevo fare altrimenti
 
-map.on("click", async function (e){
-    let { lat, lng } = e.latlng;
+    map.on("click", async function (e) {
+        let { lat, lng } = e.latlng;
 
-    try {
-        let data = await getWeather(lat, lng);
+        try {
+            let data = await getWeather(lat, lng);
             minTemperature = data.daily.temperature_2m_min[0];
             maxTemperature = data.daily.temperature_2m_max[0];
 
-        if (marker) {
-            map.removeLayer(marker);
+            if (marker) {
+                map.removeLayer(marker);
+            }
+            marker = L.marker([lat, lng]).addTo(map);
+            marker.bindPopup(`POSIZIONE MARKER<br>Latitude: ${lat.toFixed(5)}<br>Longitude: ${lng.toFixed(5)}<br>Min Temperature: ${minTemperature}°C<br>Max Temperature: ${maxTemperature}°C`).openPopup(); //ho usato <br>... non sapevo fare altrimenti
+        } catch (error) {
+            console.error("Errore API:", error);
         }
-        marker = L.marker([lat, lng]).addTo(map);  //POPUP MARKER
-        marker.bindPopup(`POSIZIONE MARKER<br>Latitude: ${lat.toFixed(5)}<br>Longitude: ${lng.toFixed(5)}<br>Min Temperature: ${minTemperature}°C<br>Max Temperature: ${maxTemperature}°C`).openPopup(); //non sapevo come fare oltre al <br>
+    });
+}
+
+navigator.geolocation.getCurrentPosition(async function (e) {
+    createMap(e.coords.latitude, e.coords.longitude);
+
+    try {
+        let data = await getWeather(e.coords.latitude, e.coords.longitude);
+        minTemperature = data.daily.temperature_2m_min[0];
+        maxTemperature = data.daily.temperature_2m_max[0];
+
+        if (marker) {
+            marker.bindPopup(`POSIZIONE MARKER<br>Latitude: ${e.coords.latitude.toFixed(5)}<br>Longitude: ${e.coords.longitude.toFixed(5)}<br>Min Temperature: ${minTemperature}°C<br>Max Temperature: ${maxTemperature}°C`).openPopup(); //ho usato <br>... non sapevo fare altrimenti
+        }
+
     } catch (error) {
         console.error("Errore API:", error);
     }
-})
-}
-
-navigator.geolocation.getCurrentPosition(function(e){
-
-createMap(e.coords.latitude, e.coords.longitude) //accept
-},(function(e){
-    createMap(41.9027835, 12.4963655) //reject
-    }),
-
-),
+}, function (e) {
+    createMap(41.9027835, 12.4963655);
+});
 
 
 document.querySelector("#coordinates-form").addEventListener("submit", async function (event) {
@@ -87,6 +101,120 @@ let resetButton = document.querySelector("#reset-button").addEventListener('clic
     if (marker) {
         map.removeLayer(marker);
     }
-
 })
+
+
+//grafico
+
+document.querySelector("#show-chart-button").addEventListener('click', () => {
+    let chartContainer = document.querySelector("#chart-container");
+    if (chartContainer.style.display === 'none' || chartContainer.style.display === '') {
+        chartContainer.style.display = 'block';
+        if (myChart) {
+            myChart.destroy(); 
+        }
+        createChart();
+    } else {
+        chartContainer.style.display = 'none';
+    }
+});
+
+let myChart; 
+
+function createChart() {
+    let ctx = document.getElementById("myChart").getContext("2d");
+
+    myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: chartData.labels,
+            datasets: [
+                {
+                    label: "Temperatura (°C)",
+                    data: chartData.temperature,
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                    yAxisID: "temperature-y-axis",
+                },
+                {
+                    label: "Umidità (%)",
+                    data: chartData.humidity,
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    backgroundColor: "rgba(75, 192, 192, 0.2)",
+                    yAxisID: "humidity-y-axis",
+                },
+            ],
+        },
+        options: {
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Ore",
+                    },
+                },
+                y: { 
+                    position: "left",
+                    display: true,
+                    title: {
+                        display: true,
+                        text: "Temperatura (°C) / Umidità (%)", 
+                    },
+                },
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: "top",
+                },
+            },                            
+        },
+    });
+}
+
+
+document.querySelector("#coordinates-form").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    let latitude = document.querySelector("#latitudine").value;
+    let longitude = document.querySelector("#longitudine").value;
+
+    try {
+        let data = await getWeather(latitude, longitude);
+        let temperatureData = data.daily.temperature_2m_min;
+        let humidityData = data.daily.temperature_2m_max;
+
+        let currentTime = new Date();
+        let currentHour = currentTime.getHours();
+        let totalHours = 24; 
+        let labels = [];
+        let temperaturePoints = [];
+        let humidityPoints = [];
+
+        for (let i = 0; i <= totalHours; i++) {
+            labels.push(i + ":00");
+
+            if (i <= currentHour) {
+                temperaturePoints.push(temperatureData[i]);
+                humidityPoints.push(humidityData[i]);
+            } else {
+                temperaturePoints.push(null);
+                humidityPoints.push(null);
+            }
+        }
+
+        chartData.labels = labels;
+        chartData.temperature = temperaturePoints;
+        chartData.humidity = humidityPoints;
+
+        if (myChart) {
+            myChart.destroy();
+        }
+        createChart();
+
+    } catch (error) {
+        console.error("Errore API:", error);
+    }
+});
 
